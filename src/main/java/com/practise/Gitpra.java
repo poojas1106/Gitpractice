902598d638950e2a65ad78f7f7550a4e5d9ddc84
Pooja <poojanarayanaraju06@gmail.com>
2024-12-23 09:41:04 -0500

    adding the inital code

---
+++ src/main/java/com/practise/Gitpra.java
@@ -0,0 +1,8 @@
+package com.practise;
+
+public class Gitpra {
+	public static void main(String[] args) {
+		System.err.println("Hello World");
+	}
+
+}
---
+++ src/main/java/com/practise/GitpractiseApplication.java
@@ -0,0 +1,14 @@
+package com.practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitpractiseApplication.class, args);
+		// System.err.println("Hello world");
+	}
+
+}
